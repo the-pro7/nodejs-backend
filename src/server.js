75d60a079b7e84b62fe3fc9f59dbfd8f63a14674
the-pro7 <ameyawemmanuel900@gmail.com>
2023-11-16 22:26:45 +0000

    updated connection to MongoDB

--- src/server.js
+++ src/server.js
@@ -9,18 +9,29 @@ const app = express()
 // Midleware for express app
 app.use(erroHandler)
 app.use(express.json())
+// Allow app to read front-end form data
 app.use(express.urlencoded({extended: true}))
 
 // Port for requests
 const port = process.env.PORT || 5000
 
+// Use routes middle ware
 app.use("/api/contacts", require("../routes/contactRoutes"))
 
+
+// Connect to MongoDB
 mongoose.connect(process.env.CONNECTION_STRING).then(()=> {
     console.log("connected")
     app.listen(port, () => {
         console.log(`App running on ${port}`)
     })
 }).catch(err => console.log(err.stack))
+
+// Assign variable to connection
+const connection = mongoose.connection
+
+connection.once("open", () => {
+    console.log(`MongoDb just joined the fun!!!`)
+})
 
 
